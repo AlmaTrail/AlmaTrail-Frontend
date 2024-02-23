a64ee55bdd40aec932fec437c6c4229d459e92b7
Ayush <ayushbansal095@gmail.com>
2024-02-23 08:08:50 +0530

    hero section

---
+++ src/components/navbar.tsx
@@ -0,0 +1,96 @@
+'use client';
+// Navbar.js
+import { useState } from 'react';
+import { CiMenuFries } from "react-icons/ci";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const menuItems = [
+    {
+      name: 'Home',
+      href: '#',
+    },
+    {
+      name: 'Universities',
+      href: '#',
+    },
+    {
+      name: 'Join us a Mentor?',
+      href: '#',
+    },
+    {
+      name: 'About Us',
+      href: '#',
+    },
+    {
+      name: 'Contact',
+      href: '#',
+    },
+  ];
+
+  return (
+    <div className="bg-white rounded-full py-2 fixed top-5 z-10">
+      <div className="mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center mr-40 lg:mr-80">
+          <span className="font-bold">AlmaTrail</span>
+        </div>
+        <div className="hidden lg:flex lg:items-center">
+          <ul className="flex space-x-6">
+            {menuItems.map((item) => (
+              <li key={item.name}>
+                <a
+                  href={item.href}
+                  className="text-sm font-semibold text-gray-800 hover:text-gray-900"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="hidden lg:flex">
+          <button
+            type="button"
+            className="py-1 px-5 ml-3 bg-black text-white rounded-full"
+          >
+            Sign up
+          </button>
+        </div>
+        <div className="lg:hidden">
+          <button onClick={toggleMenu} className="h-6 w-6 cursor-pointer">
+          <CiMenuFries />
+          </button>
+        </div>
+        {isMenuOpen && (
+          <div className="absolute rounded-t-lg inset-x-0 top-full mt-1 z-50 bg-white border-b border-gray-200 lg:hidden rounded-b-lg overflow-hidden">
+            <ul className="flex flex-col space-y-2 p-2">
+              {menuItems.map((item) => (
+                <li key={item.name}>
+                  <a
+                    href={item.href}
+                    className="block py-2 px-4 text-sm font-semibold text-gray-800 hover:text-gray-900"
+                  >
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <button
+              type="button"
+              className="w-full bg-black py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus:outline-none focus-visible:ring-2 focus-visible:ring-black"
+            >
+              Sign Up
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
