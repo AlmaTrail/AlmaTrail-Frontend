9a71c8ca4b499df70b7e13243b9cedb926d47b67
Ayush <ayushbansal095@gmail.com>
2024-04-08 10:28:07 +0530

    add animation & fix sizing

--- src/components/countrySection.tsx
+++ src/components/countrySection.tsx
@@ -1,20 +1,20 @@
-"use client";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { useState } from "react";
 import countries from "../data/countryData.js";
+import Image from "next/image.js";
 
 const CountrySection = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   const handleNext = () => {
     setActiveIndex((prevIndex) =>
-      prevIndex === countries.length - 8 ? 0 : prevIndex + 1
+      prevIndex === countries.length - 6 ? 0 : prevIndex + 1
     );
   };
 
   const handlePrev = () => {
     setActiveIndex((prevIndex) =>
-      prevIndex === 0 ? countries.length - 8 : prevIndex - 1
+      prevIndex === 0 ? countries.length - 6 : prevIndex - 1
     );
   };
 
@@ -26,20 +26,20 @@ const CountrySection = () => {
           style={{ width: "50px", height: "100px" }}
           className="cursor-pointer text-white"
         />
-        <div className="flex flex-wrap justify-center items-center">
+        <div className="flex flex-wrap justify-center items-center relative">
           {countries
-            .slice(activeIndex, activeIndex + 8)
+            .slice(activeIndex, activeIndex + 6)
             .map((country, index) => (
-              <div key={index} className="relative">
-                <img
+              <div key={index} className="relative group">
+                <Image
                   src={country.imageUrl}
                   width={220}
                   height={220}
                   alt={`Flag of ${country.name}`}
-                  className="rounded-full drop-shadow-2xl w-full h-full object-cover hover:opacity-0 transition duration-300 ease-in-out"
+                  className="rounded-full drop-shadow-2xl object-cover opacity-100 group-hover:opacity-5 transition duration-300 ease-in-out"
                 />
-                <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
-                  <p className="text-white text-6xl font-bold">{country.name}</p>
+                <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                  <p className="text-white text-4xl font-bold">{country.name}</p>
                 </div>
               </div>
             ))}
