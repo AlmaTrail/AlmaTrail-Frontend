eb94d254600ed8fed81464c3b86ddf28e178a5d5
Ayush Bansal <ayushbansal095@gmail.com>
2024-04-15 11:44:29 +0530

    add already partner universities section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
         <Navbar />
         <HeroSection />
       </div>
-      <UpcomingSection/>
-      <FeatureSection/>
-      <UniversitiesSection/>
+      <UpcomingSection />
+      <FeatureSection />
+      <UniversitiesSection />
     </main>
   );
 }
