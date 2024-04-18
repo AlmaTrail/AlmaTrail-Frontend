4fcef87c15e3d32e24786f61e9149486d98cd915
Ayush Bansal <ayushbansal095@gmail.com>
2024-04-18 23:59:02 +0530

    faq improvements

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -14,10 +14,6 @@ const QNA = [
     answer: "Berlin",
   },
   {
-    question: "What is the capital of Italy?",
-    answer: "Rome",
-  },
-  {
     question: "What is the capital of Portugal?",
     answer: "Lisbon",
   },
@@ -34,63 +30,64 @@ const Section5 = () => {
     setFeedback(QNA[index].answer);
   };
   return (
-    <div className="flex justify-between items-start p-8 bg-red-500 w-full gap-5">
-      <div className="w-1/2">
-        <h5 className="text-xl">Question</h5>
-        {QNA.map((qna, index) => (
+    <div className="bg-purple-950">
+      <p className="flex justify-center text-4xl text-white pt-4 font-bold">Frequently Asked Questions</p>
+      <div className="flex justify-between p-8 w-full gap-8">
+        
+        <div className="w-1/2 flex flex-col gap-2">
+          {QNA.map((qna, index) => (
+            <div
+              key={index}
+              className={`lex items-center w-full p-3 rounded-md cursor-pointer ${
+                selected === index && !isFeedback ? "bg-blue-500 text-white" : "bg-white"
+              }`}
+              onClick={() => handleSelect(index)}
+            >
+              <p>{qna.question}</p>
+            </div>
+          ))}
           <div
-            key={index}
-            className={`flex justify-between items-center w-full p-4 rounded-md cursor-pointer ${
-              selected === index && !isFeedback ? "bg-red-600 text-white" : "bg-white"
-            }`}
-            onClick={() => handleSelect(index)}
+            className={`flex justify-between items-center w-full p-4 rounded-md cursor-pointer ${isFeedback ? "bg-blue-600 text-white" : "bg-white"
+              }`}
+            onClick={() => {
+              setIsFeedback(true);
+              setFeedback("");
+            }}
           >
-            <p>{qna.question}</p>
+            <p className="cursor-pointer">Feedback</p>
           </div>
-        ))}
-        <div
-          className={`flex justify-between items-center w-full p-4 rounded-md cursor-pointer ${
-            isFeedback ? "bg-red-600 text-white" : "bg-white"
-          }`}
-          onClick={() => {
-            setIsFeedback(true);
-            setFeedback("");
-          }}
-        >
-          <p className="cursor-pointer">Feedback</p>
         </div>
-      </div>
-      <div className="w-1/2">
-        <h5 className="text-xl">Answer</h5>
-        <textarea
-          value={isFeedback ? feedback : QNA[selected].answer}
-          disabled={!isFeedback}
-          className="w-full p-4 rounded-md bg-white"
-          rows={10}
-          onChange={(e) => setFeedback(e.target.value)}
-        />
-        {isFeedback && (
-          <div className="flex justify-end items-center w-full">
-            <button
-              className="btn-cancel"
-              onClick={() => {
-                setIsFeedback(false);
-                setFeedback(QNA[selected].answer);
-              }}
-            >
-              Cancel
-            </button>
-            <button
-              className="btn-save"
-              onClick={() => {
-                QNA[selected].answer = feedback;
-                setIsFeedback(false);
-              }}
-            >
-              Save
-            </button>
-          </div>
-        )}
+        <div className="w-1/2">
+          <textarea
+            value={isFeedback ? feedback : QNA[selected].answer}
+            disabled={!isFeedback}
+            className="w-full p-4 rounded-md bg-white"
+            rows={10}
+            onChange={(e) => setFeedback(e.target.value)}
+          />
+          {isFeedback && (
+            <div className="flex justify-start items-center w-full gap-3">
+              <button
+                className="btn-cancel text-black shadow-md bg-white p-2 rounded-md hover:bg-blue-500 hover:text-white"
+                onClick={() => {
+                  setIsFeedback(false);
+                  setFeedback(QNA[selected].answer);
+                }}
+              >
+                Cancel
+              </button>
+              <button
+                className="btn-save text-black shadow-md bg-white p-2 rounded-md hover:bg-blue-500 hover:text-white"
+                onClick={() => {
+                  QNA[selected].answer = feedback;
+                  setIsFeedback(false);
+                }}
+              >
+                Save
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
