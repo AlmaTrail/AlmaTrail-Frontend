aba809eb10c199d860a71ff7d4b43727999aeac7
Ayush Bansal <ayushbansal095@gmail.com>
2024-05-22 22:26:52 +0530

    add responsive to some sections

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -6,23 +6,23 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-5 mt-8">
-      <div className="flex flex-col gap-4 sm:justify-center sm:items-center md:justify-center md:items-center lg:gap-8">
-        <p className="text-white font-gilroy-bold font-bold sm:text-4xl sm:text-center md:text-center md:text-5xl lg:text-6xl mt-10">
-          Bridge the Gap<br /> Between<br className="lg:flex hidden" />Dream School and{" "}
+    <div className="flex flex-col lg:flex-row sm:p-8 lg:justify-between items-center gap-5 lg:mt-8">
+      <div className="flex flex-col gap-4 sm:items-left md:justify-center md:items-center lg:gap-8">
+        <p className="text-white font-gilroy-bold font-bold sm:text-4xl sm:text-left md:text-center md:text-5xl lg:text-6xl mt-10">
+          Bridge the Gap<br /> Between<br className="flex" />Dream School and{" "}
           <br className="lg:flex hidden text-blue-500" />{" "}
           <span className="text-yellow-500"> Reality.<br /></span>
         </p>
-        <p className="text-lg font-gilroy-regular sm:text-base sm:text-center md:text-center text-white font-medium mb-0">
+        <p className="text-lg font-gilroy-regular sm:text-left md:text-center text-white font-medium mb-0">
           Turn your passion and knowledge into a thriving business.<br />Help your audience get ahead in life.<br />
         </p>
-        <div className="flex justify-around">
-          <button className="py-2 w-full bg-white rounded-md text-[#1d0828] font-gilroy-bold shadow-sm transition duration-200 hover:bg-[#1d0828] hover:text-white border-2 border-transparent hover:border-white mr-5">
-            <div>
+        <div className="flex sm:flex-col justify-around">
+          <button className="py-2 w-full sm:my-4 bg-white rounded-md text-[#1d0828] font-gilroy-bold shadow-sm transition duration-200 hover:bg-[#1d0828] hover:text-white border-2 border-transparent hover:border-white mr-5">
+            <div >
               <Link href={'/user_signup'}>Register Here</Link>
             </div>
           </button>
-          <button className="py-2 w-full hover:bg-white rounded-md hover:text-[#1d0828] font-gilroy-bold shadow-sm transition duration-200 bg-[#1d0828] text-white border-2 border-white">
+          <button className="py-2 w-full sm:my-2 hover:bg-white rounded-md hover:text-[#1d0828] font-gilroy-bold shadow-sm transition duration-200 bg-[#1d0828] text-white border-2 border-white">
             <div>
               <Link href={'/user_signup'}>Sign up</Link>
             </div>
--- src/components/introSection.tsx
+++ src/components/introSection.tsx
@@ -9,15 +9,15 @@ const IntroSection = () => {
     return (
         <div>
             <div className='flex flex-row p-8'>
-                <Image src={IntroImage} className="w-1/2 h-2/3" alt="intro_image" />
-                <div className='flex flex-col mt-12 w-5/12'>
-                    <p className='text-3xl font-gilroy-bold text-[#1d0828]'>We Provide you the best platform to start</p>
-                    <p className='mt-7 text-slate-500 text-sm font-gilroy-regular'>We offer you the best platform to make some side income while working you also got the satisfaction by helping the new dreamers
+                <Image src={IntroImage} className="w-1/2 h-2/3 sm:hidden" alt="intro_image" />
+                <div className='flex flex-col mt-12 lg:w-5/12'>
+                    <p className='text-3xl font-gilroy-bold text-[#1d0828] sm:text-center'>We Provide you the best platform to start</p>
+                    <p className='mt-7 text-slate-500 text-sm font-gilroy-regular sm:text-center'>We offer you the best platform to make some side income while working you also got the satisfaction by helping the new dreamers
                         this is just the starting, we have lot more for you, this is the best oppurtunity for you we have some special offers for the
                         early participants. We will give the great offers to the early registeration. Stay tuned.</p>
                 </div>
             </div>
-            <div className='flex flex-row py-7 px-10 justify-center gap-32'>
+            <div className='flex flex-row sm:flex-col py-7 px-10 justify-center sm:items-center sm:gap-16 lg:gap-32'>
                 <div className='w-52'>
                     <div className="mt-3 flex flex-col justify-center items-center">
                         <div>
